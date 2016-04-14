14ef1fa27cd540b9a24100e51a772ddfcecb8cbb
Warren Scipio <warrenscipio@gmail.com>
2016-04-14 09:55:42 -0500

    removed prog1 and 2

--- KU EECS/Programming 1 EECS168/Android/Lab5/Lab5_starter/gen/edu/ku/eecs168/lab5/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.ku.eecs168.lab5;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- KU EECS/Programming 1 EECS168/Lab01/Scipio-2709414-Lab-01/Ex1.java
+++
@@ -1,7 +0,0 @@
-public class Ex1{
-
-        public static void main(String[] args){
-               System.out.println("This is my first lab exercise");
-        }
-}
-
